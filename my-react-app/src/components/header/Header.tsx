b15147e9eba50f4d3cb1474b9d63d0cfca6151a8
Наталия Сенюшкина <senuskinanatalia@gmail.com>
2025-04-23 19:51:54 +0300

    задание 2..

--- my-react-app/src/components/header/Header.tsx
+++ my-react-app/src/components/header/Header.tsx
@@ -1,7 +1,23 @@
 export const Header = () => {
     return (
-    <>
-    <header>это header</header>
-    </>    
+        <header className="header">
+        <div className="left">
+            <img src="img/header/logo.svg" alt="" className="header__logo"/>
+
+            <nav className="header__nav">
+                <ul className="header-list">
+                    <li className="header-list__item">STUDIO</li>
+                    <li className="header-list__item">COMMUNITY</li>
+                    <li className="header-list__item">SOUNDS</li>
+                    <li className="header-list__item">PLUGINS</li>
+                    <li className="header-list__item">BLOG</li>
+                </ul>
+            </nav>
+        </div>
+        <div className="header__buttons">
+            <a href="#" className="header__button-log">LOG IN</a>
+            <button className="header__button-sign" id="openModal">SIGN UP</button>
+        </div>
+    </header>
     )
 }
