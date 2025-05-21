f2be761e299d84b6e96a11f097ae650a5657e285
Наталия Сенюшкина <senuskinanatalia@gmail.com>
2025-05-21 19:59:38 +0300

    Задание 2++

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,15 +1,26 @@
-//import Message from './Message'
-import './App.css'
-import { Layout } from './components/Layout';
+import { Layout } from "./components/Layout";
+import { MainBlock } from "./components/MainBlock/MainBlock";
+import { SectionFeatured } from "./components/SectionFeatured/SectionFeatured";
+import { SectionSounds } from "./components/SectionSounds/SectionSounds";
+import { SectionPlugins } from "./components/SectionPlugins/SectionPlugins";
+import { SectionStudio } from "./components/SectionStudio/SectionStudio";
+import { SectionReviews } from "./components/SectionReviews/SectionReviews";
+import { SectionAdvantages } from "./components/SectionAdvantages/SectionAdvantages";
+import { SectionJobs } from "./components/SectionJobs/SectionJobs";
 
 function App() {
-/*  const messageText = "Привет, мир!"; // Константа с текстом*/
-
   return (
-    <>
-      <Layout/>
-    </>
-  )
+    <Layout>
+      <MainBlock />
+      <SectionFeatured />
+      <SectionSounds />
+      <SectionPlugins />
+      <SectionStudio />
+      <SectionReviews />
+      <SectionAdvantages />
+      <SectionJobs />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/components/Layout.tsx
+++ my-react-app/src/components/Layout.tsx
@@ -1,31 +1,20 @@
 import { Header } from "./header/Header";
 import { Modal } from "./modal/Modal";
-//import { Preloader } from "./preloader/Preloader";
-import { MainBlock } from "./MainBlock/MainBlock";
-import { SectionFeatured } from "./SectionFeatured/SectionFeatured";
-import { SectionSounds } from "./SectionSounds/SectionSounds";
-import { SectionPlugins } from "./SectionPlugins/SectionPlugins";
-import { SectionStudio } from "./SectionStudio/SectionStudio";
-import { SectionReviews } from "./SectionReviews/SectionReviews";
-import { SectionAdvantages } from "./SectionAdvantages/SectionAdvantages";
-import { SectionJobs } from "./SectionJobs/SectionJobs";
 import { Footer } from "./footer/Footer";
 
-export const Layout = () => {
-    return (
-        <>
-            <Header/>
-            <Modal/>
-            {/*<Preloader/>*/}
-            <MainBlock/>
-            <SectionFeatured/>
-            <SectionSounds/>
-            <SectionPlugins/>
-            <SectionStudio/>
-            <SectionReviews/>
-            <SectionAdvantages/>
-            <SectionJobs/>
-            <Footer/>
-        </>
-    )
+interface LayoutProps {
+  children: React.ReactNode;  // Контент страницы (MainBlock, SectionFeatured и т. д.)
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <Modal /> 
+      <main>       
+        {children}
+      </main>
+      <Footer />
+      </>
+  )
 }
